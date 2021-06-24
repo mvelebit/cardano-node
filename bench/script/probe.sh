eaa144ae210b19c9bd7a36b91228bf5ec2649b30
Kosyrev Serge <serge.kosyrev@iohk.io>
2021-06-24 22:14:01 +0300

    CAD-2907 bench:  add the latency probe script

---
+++ bench/script/probe.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+cls
+hostmap=$(jq '
+  .public_ip
+  | values
+  | map({ "\(.hostname)": .public_ip})
+  | add' meta.json)
+
+nixops ssh-for-each --parallel -- "
+   self=\$(hostname)
+
+  function probe() {
+      local host=\$1 ip=\$2
+
+      ping -qAc21 \$ip                       |
+      grep 'rtt\|transmitted'                |
+      sed 's_, \|/_\n_g'                     |
+      sed 's_ packets transmitted\| received\| packet loss\|time \|rtt min\|avg\|max\|mdev = \|ipg\|ewma \| ms\|ms\|%__g'         |
+      grep -v '^$'                           |
+      jq '{ source: \"'\$self'\"
+          , target: \"'\$host'\"
+
+          , sent:          .[0]
+          , received:      .[1]
+          , percents_lost: .[2]
+          , duration_ms:   .[3]
+          , ipg:           .[8]
+          , ewma:          .[9]
+
+          , rtt: { min: .[4], avg: .[5], max: .[6], mdev: .[7] }
+          }' --slurp --compact-output
+   }
+
+   hostmap=${hostmap@Q}
+
+   for host in \$(jq 'keys | .[]' <<<\$hostmap --raw-output)
+   do ip=\$(jq '.[\$host]' --arg host \$host <<<\$hostmap --raw-output)
+      echo \"probe \$self -> \$host \$ip\"
+   done"
